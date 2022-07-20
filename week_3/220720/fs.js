0e321207a7c03c6455641e29a84b197c4d7454d9
sionyeom <duatldhs5555@gmail.com>
2022-07-20 15:51:22 +0900

    study: fs

--- week_3/220720/fs.js
+++ week_3/220720/fs.js
@@ -1,8 +1,47 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-fs.readFile("./text.txt", (err, data) => {
-  if (err) throw err;
-  // 에러 출력 및 break;
+// fs.readFile("./text.txt", (err, data) => {
+//   if (err) throw err;
+//   // 에러 출력 및 break;
 
-  console.log(data.toString());
-});
+//   console.log(data.toString());
+// });
+
+// fs
+//   .writeFile("./write.txt", "안녕")
+//   .then(() => {
+//     console.log("작성 완료");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// fs
+//   .copyFile("./write.txt", "./copied.txt")
+//   .then(() => {
+//     console.log("복사 완료");
+//   })
+//   .catch(err => {
+//     throw err;
+//   });
+
+async function exec() {
+  await fs
+    .writeFile("./write.txt", "안녕")
+    .then(() => {
+      console.log("작성 완료");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  await fs
+    .copyFile("./write.txt", "./copied.txt")
+    .then(() => {
+      console.log("복사 완료");
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+exec();
