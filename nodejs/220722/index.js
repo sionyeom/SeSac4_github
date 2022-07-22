22f5f31ba2e80ef34b14317d5e0149a045d8ec53
sionyeom <duatldhs5555@gmail.com>
2022-07-22 16:11:13 +0900

    study: package install

---
+++ nodejs/220722/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use("/abc", express.static("public"));
+// 가상 경로 /abc를 만들어준다
+// 정적 파일 관리
+
+app.get("/", (req, res) => {
+  res.render("test");
+  // ejs 파일일 이용할 것이고, views 폴더에 자동적으로 접근한다.
+});
+
+app.listen(port, () => {
+  console.log("Server Port : ", port);
+});
