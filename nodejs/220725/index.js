ceeb85827a0fb0acd0fee9c01bfc95c7e56337a7
sionyeom <duatldhs5555@gmail.com>
2022-07-25 18:25:03 +0900

    study: practice 33 complete

--- nodejs/220725/index.js
+++ nodejs/220725/index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 8080;
 const bodyParser = require("body-parser");
 const fs = require("fs");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // 렌더링
 app.get("/", function(req, res) {
   res.render("index");
@@ -19,6 +21,9 @@ app.get("/practice31", function(req, res) {
 });
 app.get("/practice32", function(req, res) {
   res.render("practice_32");
+});
+app.get("/practice33", function(req, res) {
+  res.render("practice_33");
 });
 
 // get or post 값 받는 페이지
@@ -46,6 +51,23 @@ app.post("/practice_receive_practice32", function(req, res) {
     //file written successfully
   });
   res.render("practice_receive_practice32", req.body);
+});
+
+app.post("/practice_receive_practice33", function(req, res) {
+  // 1. info.txt의 내용을 //로 구분하여 배열에 담는다.
+  const data = fs
+    .readFileSync("info.txt", { encoding: "utf8", flag: "r" })
+    .split("//");
+  // 2. res에 받아온 값과 배열의 첫번째 값과 마지막 값을 비교하는 조건문을 작성한다.
+  if (data[0] == req.body.id && data[2] == req.body.pw) {
+    console.log("로그인에 성공했습니다.");
+  } else if (data[0] == req.body.id && data[2] != req.body.pw) {
+    console.log("비밀번호를 확인해주세요.");
+  } else if (data[0] != req.body.id && data[2] == req.body.pw) {
+    console.log("아이디를 확인해주세요.");
+  } else {
+    console.log("아이디와 비밀번호를 확인해주세요.");
+  }
 });
 
 app.get("/receive", function(req, res) {
