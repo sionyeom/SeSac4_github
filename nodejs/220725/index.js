a1a4b7502a010b5219485a64fca6dea2050301ee
sionyeom <duatldhs5555@gmail.com>
2022-07-25 15:40:47 +0900

    study: practice31,32

--- nodejs/220725/index.js
+++ nodejs/220725/index.js
@@ -7,14 +7,37 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+// 렌더링
 app.get("/", function(req, res) {
   res.render("index");
+});
+app.get("/practice30", function(req, res) {
+  res.render("practice_30");
+});
+app.get("/practice31", function(req, res) {
+  res.render("practice_31");
+});
+app.get("/practice32", function(req, res) {
+  res.render("practice_32");
+});
+
+// get or post 값 받는 페이지
+app.get("/practice_receive", function(req, res) {
+  console.log("receive-get");
+  console.log(req.query);
+  res.render("practice_receive", req.query);
+});
+app.post("/practice_receive_post", function(req, res) {
+  console.log("practice_receive_post");
+  console.log(req.body);
+  res.render("practice_receive_post", req.body);
 });
 
 app.get("/receive", function(req, res) {
   console.log("receive-get");
-  res.render("index");
+  console.log(req.query);
+  console.log(req);
+  res.render("receive", req.query);
 });
 
 app.post("/receive", function(req, res) {
