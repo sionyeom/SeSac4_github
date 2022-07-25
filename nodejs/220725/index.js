f9deccace1ebb2e73292fbdcbeb918c08ac055c7
sionyeom <duatldhs5555@gmail.com>
2022-07-25 13:42:12 +0900

    study: nodejs init

--- nodejs/220725/index.js
+++ nodejs/220725/index.js
@@ -1,13 +1,25 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser().json());
+app.use(bodyParser.json());
 
-app.get("/", function(req, res) {});
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/receive", function(req, res) {
+  console.log("receive-get");
+  res.render("index");
+});
+
+app.post("/receive", function(req, res) {
+  console.log("receive-post");
+  res.render("index");
+});
 
 app.listen(port, () => {
   console.log("Server Port : ", port);
