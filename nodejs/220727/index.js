b34f0a94b6e650ff9a258fa282c6399cd6c2eccd
sionyeom <duatldhs5555@gmail.com>
2022-07-27 17:53:55 +0900

    study: completed practices

--- nodejs/220727/index.js
+++ nodejs/220727/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,8 +13,50 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/receive", (req, res) => {
+app.get("/practice33", (req, res) => {
+  res.render("practice33");
+});
+app.get("/practice34", (req, res) => {
+  res.render("practice34");
+});
+app.get("/practice35-1", (req, res) => {
+  res.render("practice35-1");
+});
+app.get("/practice35-2", (req, res) => {
+  res.render("practice35-2");
+});
+app.get("/practice35-3", (req, res) => {
+  res.render("practice35-3");
+});
+app.get("/practice35-4", (req, res) => {
+  res.render("practice35-4");
+});
+
+app.get("/receive", (req, res) => {
+  console.log(req.query);
+  let name = req.query.name;
+  let msg = req.query.name + " GET 안녕";
+  res.send({ name: name, message: msg });
+});
+app.get("/receive-practice33", (req, res) => {
+  let data = req.query;
+  console.log(req.query);
+  res.send(req.query);
+});
+app.post("/receive-practice34", (req, res) => {
   console.log(req.body);
+  // 1. info.txt의 내용을 //로 구분하여 배열에 담는다.
+  const data = fs
+    .readFileSync("info.txt", { encoding: "utf8", flag: "r" })
+    .split("//");
+  // 2. req.body로 받아온 값들을 저장한다.
+  let id = req.body.id;
+  let pw = req.body.pw;
+  // 3. 다시 프론트로 데이터를 뿌려준다.
+  res.send({ data: data, id: id, pw: pw });
+});
+
+app.post("/receive", (req, res) => {
   let name = req.body.name;
   let msg = req.body.name + " 안녕";
   res.send({ name: name, message: msg });
