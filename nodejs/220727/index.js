1441dbc7ef707164c0b98f56eaad43ac245495be
sionyeom <duatldhs5555@gmail.com>
2022-07-27 13:40:53 +0900

    study: add index

---
+++ nodejs/220727/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log("Server Port : ", port);
+});
