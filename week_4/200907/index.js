db61cfc232bf318ba7412f160616af770095ecf1
sionyeom <duatldhs5555@gmail.com>
2022-09-11 10:48:21 +0900

    feat: add list

--- week_4/200907/index.js
+++ week_4/200907/index.js
@@ -23,42 +23,52 @@ app.get("/example3", (req, res) => {
 
 let count = 1;
 let userCount = 0;
-const userArr = [];
-io.on("connect", function(socket) {
+let userArr = [];
+io.on("connect", function (socket) {
+  // 클릭 시 이벤트 예제
+  socket.on("click", (data) => {
+    console.log("client : ", data);
+    socket.emit("clickResponse", socket.id);
+  });
+
   console.log("connected");
   // socket.emit("hello", "server hello");
   // 입장 시
   let name = `사용자_${count++}`;
   userCount++;
-  console.log(userCount);
+
+  // 새로운 유저 입장 시 배열에 추가
   userArr.push({ name: name, id: socket.id });
-  console.log(userArr);
+
+  // 새로운 유저에게 이름 부여
+  io.to(socket.id).emit("getName", { name: name, dm: socket.id });
+
   // 유저 객체에 할당
   socket.emit("clickResponse", socket.id);
-  io.emit("name", { name: name, dm: socket.id });
-  io.to(socket.id).emit("getName", { name: name, dm: socket.id });
-  socket.on("click", data => {
-    console.log("client : ", data);
-    socket.emit("clickResponse", socket.id);
-    // io.emit("clickResponse", "io success");
-  });
 
-  socket.on("sendMsg", data => {
+  // 다른 사람 입장시
+  io.emit("name", { name: name, userArr: userArr });
+
+  // 메시지 보내기
+  socket.on("sendMsg", (data) => {
     console.log(`${name} : ${data.message}`);
     io.emit("getMsg", data);
   });
-  socket.on("changeName", data => {
+
+  // 이름 변경
+  socket.on("changeName", (data) => {
     name = data.after;
     let beforeName = data.before;
     let dataArr = {
       before: beforeName,
-      after: name
+      after: name,
     };
     io.to(socket.id).emit("getName", { name: dataArr.after, dm: socket.id });
     io.emit("change name", dataArr);
   });
 
-  socket.on("sendDm", data => {
+  // 개인 메시지 보내기
+  socket.on("sendDm", (data) => {
     // 보내는 이
     let from = data.from;
     // 받는 이
@@ -69,14 +79,15 @@ io.on("connect", function(socket) {
     io.to(data.to).emit("getDm", { from: from, msg: msg });
   });
 
-  socket.on("disconnect", reason => {
+  // 유저 나갔을 경우 처리
+  socket.on("disconnect", (reason) => {
     userCount--;
     console.log(userCount);
-    io.emit("alertDisconnect", { name: name, dm: socket.id });
+    userArr = userArr.filter((user) => {
+      return user.id !== socket.id;
+    });
+    io.emit("alertDisconnect", { name: name, dm: socket.id, userArr: userArr });
   });
-  // if (userCount++ || userCount--) {
-  //   console.log(userCount);
-  // }
 });
 
 http.listen(8000, () => {
